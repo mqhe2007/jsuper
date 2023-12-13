7d63f549f20ddd783c8dbc3d8dc4d5c0764e24cb
孟庆贺 <122274389@qq.com>
2023-12-13 16:47:56 +0800

    同时支持esm和cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: "esm",
+  format: ["esm", "cjs"],
   dts: true,
   // experimentalDts: true,
   sourcemap: true,
